f3bca84955cadfea7a25e11c64385e39d7d278f5
Artem Husar <artemhusar2007@gmail.com>
2026-09-02 14:41:51 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,15 +4,15 @@ from typing import Any
 class Dictionary:
     def __init__(self) -> None:
         self.dictionary = [()] * 8
-        self.count = 0
+        self.length = 0
 
     def __setitem__(self, key: Any, value: Any) -> None:
         index = hash(key) % len(self.dictionary)
 
-        if self.count >= len(self.dictionary) * (2 / 3):
+        if self.length >= len(self.dictionary) * (2 / 3):
             old_dict = self.dictionary.copy()
             self.dictionary = [()] * len(self.dictionary) * 2
-            self.count = 0
+            self.length = 0
 
             index = hash(key) % len(self.dictionary)
 
@@ -23,7 +23,7 @@ class Dictionary:
         if self.dictionary[index] and self.dictionary[index][0] != key:
             for _ in range(len(self.dictionary)):
                 if not self.dictionary[index]:
-                    self.count += 1
+                    self.length += 1
                     break
                 if self.dictionary[index] and self.dictionary[index][0] == key:
                     self.dictionary[index] = (key, hash(key), value)
@@ -36,7 +36,7 @@ class Dictionary:
             self.dictionary[index] = (key, hash(key), value)
         else:
             if not self.dictionary[index]:
-                self.count += 1
+                self.length += 1
             self.dictionary[index] = (key, hash(key), value)
 
     def __getitem__(self, key: Any) -> Any:
@@ -55,4 +55,8 @@ class Dictionary:
         raise KeyError(f"Key '{key}' not found")
 
     def __len__(self) -> int:
-        return self.count
+        return self.length
+
+    def clear(self) -> None:
+        self.dictionary = [()] * len(self.dictionary)
+        self.length = 0
